3c7495016bab9cff25606cb25c28493826c13f7e
AlexProg <k_alex91@mail.ru>
2020-09-09 23:42:06 +0400

    init: task solution

---
+++ 8_kyu/reversed strings.js
@@ -0,0 +1,13 @@
+/*
+Complete the solution so that it reverses the string passed into it.
+'world'  =>  'dlrow'
+ */
+
+function solution(str){
+    let reverseStr = 'j';
+    for (let i = 0; i <= str.length; i++) {
+        reverseStr = str[i]; 
+    }
+
+    return reverseStr;
+  }
